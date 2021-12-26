dc9d13393cf719da5a4f375e0f2d6a7e34d17283
Van Hsieh <van.hsieh1@gmail.com>
2021-12-26 15:27:50 -0800

    bug fix

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -1,13 +1,70 @@
 import React from 'react';
+import axios from 'axios';
 import OverviewCss from './style/overview.css';
+import Category from './subcomponents/Category.jsx';
+import Description from './subcomponents/Description.jsx';
+import Ratings from './subcomponents/Ratings.jsx';
+import Title from './subcomponents/Title.jsx';
 
-const Overview = (props) => (
-  <div>
-    <hr />
-    <h1 className="title">This is the Overview Div</h1>
-    <div>Product id = {props.productId}</div>
-    <hr />
-  </div>
-);
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      productId: this.props.productId,
+      product: {},
+      styles: [],
+      currStyle: {},
+      reviews: [{'rating': 5}]
+    };
+
+    this.getProductInfo = this.getProductInfo.bind(this);
+    this.getStylesInfo = this.getStylesInfo.bind(this);
+    this.getReviewsInfo = this.getReviewsInfo.bind(this);
+  }
+
+  componentDidMount() {
+    this.getProductInfo();
+    this.getStylesInfo();
+    this.getReviewsInfo();
+  }
+
+  getProductInfo() {
+    axios.get(`/products/${this.props.productId}`)
+      .then(response => this.setState({
+        product: response.data
+      }));
+  }
+
+  getStylesInfo() {
+    axios.get(`/products/${this.props.productId}/styles`)
+      .then(response => this.setState({
+        styles: response.data.results,
+        currStyle: response.data.results[0]
+      }));
+  }
+
+  getReviewsInfo() {
+    axios.get(`/reviews?product_id=${this.props.productId}`)
+      .then(response => this.setState({
+        reviews: response.data.results
+      }));
+  }
+
+  render() {
+    return (
+      <div>
+        <hr />
+        <h1 className="title">This is the Overview Div</h1>
+        <div>Product id {this.state.productId}</div>
+        <Category product={this.state.product} />
+        <Description product={this.state.product} />
+        <Title product={this.state.product} />
+        <Ratings reviews={this.state.reviews} />
+        <hr />
+      </div>
+    );
+  }
+}
 
 export default Overview;
--- client/src/components/Overview/subcomponents/Title.jsx
+++ client/src/components/Overview/subcomponents/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Title = ({ product }) => (
   <div>
-    <div>{product.slogan}</div>
+    <div>{product.name}</div>
   </div>
 );
 
