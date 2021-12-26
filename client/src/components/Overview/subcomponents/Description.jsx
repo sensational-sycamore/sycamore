573952dae1e2e5dfaaec207d56d296712f189336
Van Hsieh <van.hsieh1@gmail.com>
2021-12-26 13:59:36 -0800

    set up structure of Overview direct child components Category, Description, and Title

---
+++ client/src/components/Overview/subcomponents/Description.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Description = () => (
+  <div>
+    <h1>Description</h1>
+  </div>
+);
+
+export default Description;
---
+++ client/src/components/Overview/subcomponents/Ratings.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Ratings = () => (
+  <div>
+    <h1>Ratings</h1>
+  </div>
+);
+
+export default Ratings;
