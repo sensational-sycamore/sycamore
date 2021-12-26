d15f205c503395fccd69817d39bec57b62ecde4c
Van Hsieh <van.hsieh1@gmail.com>
2021-12-26 14:33:16 -0800

    conplete Description.jsx

--- client/src/components/Overview/subcomponents/Category.jsx
+++ client/src/components/Overview/subcomponents/Category.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Category = ({ product }) => (
   <div>
-    <h1>Category</h1>
     <div>{product.category}</div>
   </div>
 );
--- client/src/components/Overview/subcomponents/Description.jsx
+++ client/src/components/Overview/subcomponents/Description.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const Description = () => (
+const Description = ({ product }) => (
   <div>
     <h1>Description</h1>
+    <div>{product.slogan}</div>
+    <div>{product.description}</div>
   </div>
 );
 
