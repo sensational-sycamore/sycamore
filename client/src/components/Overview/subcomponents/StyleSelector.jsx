2d995e095c4602f6948b1c84c4e226e36bc310bd
Van Hsieh <van.hsieh1@gmail.com>
2021-12-26 17:42:46 -0800

    refactor StyleSelector to functional component

--- client/src/components/Overview/subcomponents/StyleSelector.jsx
+++ client/src/components/Overview/subcomponents/StyleSelector.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 
-class StyleSelector extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const { styles, currStyle, handleStyleSelect} = this.props;
-    return (
-      <div>
-        <div><b>STYLE {'>'} </b> {currStyle.name}</div>
-        {styles.map(style => (
-          <div key={style.style_id}>
-            <img src={style.photos[0].thumbnail_url} alt="thumbnail" onClick={() => handleStyleSelect(style)}/>
-          </div>
-        ))}
+const StyleSelector = ({ styles, currStyle, handleStyleSelect }) => (
+  <div>
+    <div><b>STYLE {'>'} </b> {currStyle.name}</div>
+    {styles.map(style => (
+      <div key={style.style_id}>
+        <img src={style.photos[0].thumbnail_url} alt="thumbnail" onClick={() => handleStyleSelect(style)}/>
       </div>
-    );
-  }
-}
+    ))}
+  </div>
+);
 
 export default StyleSelector;
