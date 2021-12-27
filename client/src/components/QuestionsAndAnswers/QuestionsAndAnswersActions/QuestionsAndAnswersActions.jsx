7e6510ff0714b87cab5bffaedc725c2e1927dadd
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2021-12-26 20:38:31 -0800

    added questions and answers components

---
+++ client/src/components/QuestionsAndAnswers/QuestionsAndAnswersActions/QuestionsAndAnswersActions.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './QuestionsAndAnswersActions.scss';
+
+const QuestionsAndAnswersActions = () => {
+  return (
+    <p>QuestionsAndAnswersActions</p>
+  );
+};
+
+export default QuestionsAndAnswersActions;
