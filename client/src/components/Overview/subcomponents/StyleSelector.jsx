a9ea7453fa44a15f21bcfd0f85b551897e9cab44
Van Hsieh <van.hsieh1@gmail.com>
2021-12-28 15:56:57 -0800

    complete basic style selector css

--- client/src/components/Overview/subcomponents/StyleSelector.jsx
+++ client/src/components/Overview/subcomponents/StyleSelector.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import css from '../styles/styleSelector.module.css';
 
 const StyleSelector = ({ styles, currStyle, handleStyleSelect }) => (
   <div>
     <div><b>STYLE {'>'} </b> {currStyle.name}</div>
-    {styles.map(style => (
-      <div key={style.style_id}>
-        <img src={style.photos[0].thumbnail_url} alt="thumbnail" onClick={() => handleStyleSelect(style)}/>
-      </div>
-    ))}
+    <div className={css.styleImageContainer}>
+      {styles.map(style => (
+        <img key={style.style_id} className={css.styleImage} src={style.photos[0].thumbnail_url} alt="thumbnail" onClick={() => handleStyleSelect(style)}/>
+      ))}
+    </div>
   </div>
 );
 
