db5b69c8d17f795596e02069c598630498126c2b
Van Hsieh <van.hsieh1@gmail.com>
2021-12-29 15:54:23 -0800

    added ratings and review css

--- client/src/components/Overview/subcomponents/Ratings.jsx
+++ client/src/components/Overview/subcomponents/Ratings.jsx
@@ -29,7 +29,7 @@ const Ratings = ({ reviews }) => {
   return (
     <div>
       { starArr }
-      <a href="#ratings-and-reviews">Read all reviews</a>
+      <a className={css.reviewLink} href="#ratings-and-reviews">Read all reviews</a>
     </div>
   );
 };
