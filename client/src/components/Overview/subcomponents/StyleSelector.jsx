d5aec425d6967b4c7caa42c8e4868a56815d90b9
Van Hsieh <van.hsieh1@gmail.com>
2021-12-29 17:23:30 -0800

    added flex wrap to style selector css to make styles overflow to different rows

--- client/src/components/Overview/subcomponents/StyleSelector.jsx
+++ client/src/components/Overview/subcomponents/StyleSelector.jsx
@@ -4,9 +4,11 @@ import css from '../styles/styleSelector.module.css';
 const StyleSelector = ({ styles, currStyle, handleStyleSelect }) => (
   <div>
     <h4><b>STYLE {'>'} </b> {currStyle.name}</h4>
-    <div className={css.styleImageContainer}>
+    <div className={css.styleContainer}>
       {styles.map(style => (
-        <img key={style.style_id} className={css.styleImage} src={style.photos[0].thumbnail_url} alt="thumbnail" onClick={() => handleStyleSelect(style)}/>
+        <div className={css.styleImageContainer}>
+          <img key={style.style_id} className={css.styleImage} src={style.photos[0].thumbnail_url} alt="thumbnail" onClick={() => handleStyleSelect(style)}/>
+        </div>
       ))}
     </div>
   </div>
