f2f26f2c0f9471c45eb859fae66ece150cf02065
Van Hsieh <van.hsieh1@gmail.com>
2022-01-01 22:47:12 -0800

    not much changed

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -24,7 +24,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Navbar />
+        {/* <Navbar /> */}
         <Overview productId={this.state.productId}/>
         <Related productId={this.state.productId} changeProductId={this.changeProductId}/>
         <QuestionsAndAnswers productId={this.state.productId}/>
