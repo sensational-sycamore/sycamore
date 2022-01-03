80d88aab189aeebffdc3b97b132e944e32572886
Van Hsieh <van.hsieh1@gmail.com>
2022-01-03 12:23:19 -0800

    added incomplete dom test

---
+++ client/src/tests/Overview/dom.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
+import StyleSelector from '../../components/Overview/subcomponents/StyleSelector.jsx';
+
+
+afterEach(cleanup);
+
