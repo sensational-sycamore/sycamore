e7bda59c0656000d6abe34a920a75644b3e6b2b9
Van Hsieh <van.hsieh1@gmail.com>
2022-01-05 08:53:42 -0800

    added currStyle for Price component snapshot test

--- client/src/tests/Overview/styleselector.test.js
+++ client/src/tests/Overview/styleselector.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { act } from 'react-dom/test-utils';
 import StyleSelector from '../../components/Overview/subcomponents/StyleSelector.jsx';
 
-describe('StyleSelector', () => {
+describe('StyleSelector',  () => {
   let container;
 
   beforeEach(() => {
@@ -16,7 +16,7 @@ describe('StyleSelector', () => {
     container = null;
   });
 
-  var fakeStyles = {'product_id':'63610','results':[{'style_id':391652,'name':'Black Lenses & Black Frame','original_price':'69.00','sale_price':null,'default?':false,'photos':[{'thumbnail_url':null,'url':null}],'skus':{'null':{'quantity':null,'size':null}}},{'style_id':391653,'name':'Black Lenses & Gold Frame','original_price':'69.00','sale_price':null,'default?':true,'photos':[{'thumbnail_url':null,'url':null}],'skus':{'null':{'quantity':null,'size':null}}},{'style_id':391654,'name':'Gold Lenses & Black Frame','original_price':'69.00','sale_price':null,'default?':false,'photos':[{'thumbnail_url':null,'url':null}],'skus':{'null':{'quantity':null,'size':null}}},{'style_id':391655,'name':'Gold Lenses & Gold Frame','original_price':'69.00','sale_price':null,'default?':false,'photos':[{'thumbnail_url':null,'url':null}],'skus':{'null':{'quantity':null,'size':null}}}]};
+  var fakeStyles = {'product_id': '63610', 'results': [{'style_id': 391652, 'name': 'Black Lenses & Black Frame', 'original_price': '69.00', 'sale_price': null, 'default?': false, 'photos': [{'thumbnail_url': null, 'url': null}], 'skus': {'null': {'quantity': null, 'size': null}}}, {'style_id': 391653, 'name': 'Black Lenses & Gold Frame', 'original_price': '69.00', 'sale_price': null, 'default?': true, 'photos': [{'thumbnail_url': null, 'url': null}], 'skus': {'null': {'quantity': null, 'size': null}}}, {'style_id': 391654, 'name': 'Gold Lenses & Black Frame', 'original_price': '69.00', 'sale_price': null, 'default?': false, 'photos': [{'thumbnail_url': null, 'url': null}], 'skus': {'null': {'quantity': null, 'size': null}}}, {'style_id': 391655, 'name': 'Gold Lenses & Gold Frame', 'original_price': '69.00', 'sale_price': null, 'default?': false, 'photos': [{'thumbnail_url': null, 'url': null}], 'skus': {'null': {'quantity': null, 'size': null}}}]};
 
   let count = 0;
 
@@ -24,10 +24,10 @@ describe('StyleSelector', () => {
     count++;
   };
 
-  it('can render', () => {
+  it('can render',  () => {
     // Test first render and componentDidMount
     act(() => {
-      ReactDOM.render(<StyleSelector styles={fakeStyles.results} currStyle={fakeStyles.results[0]} handleStyleSelect={clickIncreaseCount}/>, container);
+      ReactDOM.render(<StyleSelector styles={fakeStyles.results} currStyle={fakeStyles.results[0]} handleStyleSelect={clickIncreaseCount}/>,  container);
     });
     const label = container.querySelector('h4');
     const img = container.querySelector('#styleImage');
