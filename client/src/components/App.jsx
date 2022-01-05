82264373f7e4e1761d182c96309783bb60147106
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2022-01-04 21:39:09 -0800

    productId query parameter

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,39 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar/Navbar.jsx';
 import Overview from './Overview/Overview.jsx';
 import Related from './Related/Related.jsx';
 import QuestionsAndAnswers from './QuestionsAndAnswers/QuestionsAndAnswers.jsx';
 import RatingsAndReviews from './RatingsAndReviews/RatingsAndReviews.jsx';
 import './App.scss';
+import { useNavigate } from 'react-router-dom';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [productId, setProductId] = useState(63613);
+  const navigate = useNavigate();
 
-    this.state = {
-      productId: 63613
-    };
+  useEffect(() => {
+    // check if there is a query parameter in the URL
+    if (window.location.search) {
+      const queryParams = new URLSearchParams(window.location.search);
+      // check if the query parameters has a value for productId
+      if (queryParams.get('productId')) {
+        // update the productId state with the productId from the URL
+        setProductId(queryParams.get('productId'));
+      } else {
+        // if no productId on the URL navigate to the productId currently on the productId state
+        navigate(`/?productId=${productId}`);
+      }
+    }
+  }, []);
 
-    this.changeProductId = this.changeProductId.bind(this);
-  }
+  // update the state productId and update URL with the new productId
+  const changeProductId = (productId) => {
+    setProductId(productId);
+    navigate(`/?productId=${productId}`);
+  };
 
-  changeProductId(productId) {
-    this.setState({ productId });
-  }
-
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Overview productId={this.state.productId}/>
-        <Related productId={this.state.productId} changeProductId={this.changeProductId}/>
-        <QuestionsAndAnswers productId={this.state.productId}/>
-        <RatingsAndReviews productId={this.state.productId}/>
-      </div>
-    );
-  }
-}
-
+  return (
+    <div>
+      <Navbar />
+      <Overview productId={productId}/>
+      <Related productId={productId} changeProductId={changeProductId}/>
+      <QuestionsAndAnswers productId={productId}/>
+      <RatingsAndReviews productId={productId}/>
+    </div>
+  );
+};
 
 export default App;
-
