943b178921ebe5eb6f5c54d497b87268001608ee
Van Hsieh <van.hsieh1@gmail.com>
2022-01-04 23:46:42 -0800

    rendering and click event test for style selector

--- client/src/components/Overview/subcomponents/StyleSelector.jsx
+++ client/src/components/Overview/subcomponents/StyleSelector.jsx
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck } from '@fortawesome/free-solid-svg-icons';
 import uniqid from 'uniqid';
 
-<<<<<<< HEAD
 const StyleSelector = ({ styles, currStyle, handleStyleSelect }) => {
 
   return (
@@ -12,26 +11,15 @@ const StyleSelector = ({ styles, currStyle, handleStyleSelect }) => {
       <h4><b>STYLE {'>'} </b> {currStyle.name}</h4>
       <div className={css.styleContainer}>
         {styles.map(style => (
-          <div className={css.styleImageContainer} key={uniqid()}>
+          <div id="styleImageContainer" className={css.styleImageContainer} key={uniqid()}>
             {currStyle.style_id === style.style_id ?
               <div className={css.checkMark}><FontAwesomeIcon icon={faCheck} size="xs"/></div> :
               null
             }
-            <img key={style.style_id} className={`${css.styleImage} ${currStyle.style_id === style.style_id ? css.selected : null}`} src={style.photos[0].thumbnail_url} alt="thumbnail" onClick={() => handleStyleSelect(style)}/>
+            <img id="styleImage" key={style.style_id} className={`${css.styleImage} ${currStyle.style_id === style.style_id ? css.selected : null}`} src={style.photos[0].thumbnail_url} alt="thumbnail" onClick={() => handleStyleSelect(style)}/>
           </div>
         ))}
       </div>
-=======
-const StyleSelector = ({ styles, currStyle, handleStyleSelect }) => (
-  <div>
-    <h4><b>STYLE {'>'} </b> {currStyle.name}</h4>
-    <div className={css.styleContainer}>
-      {styles.map(style => (
-        <div key={style.style_id} className={css.styleImageContainer}>
-          <img key={style.style_id} className={css.styleImage} src={style.photos[0].thumbnail_url} alt="thumbnail" onClick={() => handleStyleSelect(style)}/>
-        </div>
-      ))}
->>>>>>> 578ecfe (refactored ratings and review components to include keys and avoid react warnings)
     </div>
   );
 };
