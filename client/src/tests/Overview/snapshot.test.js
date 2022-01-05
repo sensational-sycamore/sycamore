4a88950ad6d53b13bc07d12ae987d7f9f2cc1a5a
Van Hsieh <van.hsieh1@gmail.com>
2022-01-05 09:57:38 -0800

    added more tests

--- client/src/tests/Overview/snapshot.test.js
+++ client/src/tests/Overview/snapshot.test.js
@@ -7,9 +7,15 @@ import Category from '../../components/Overview/subcomponents/Category.jsx';
 import Description from '../../components/Overview/subcomponents/Description.jsx';
 import Reviews from '../../components/Overview/subcomponents/Reviews.jsx';
 import Price from '../../components/Overview/subcomponents/Price.jsx';
+import Overview from '../../components/Overview/Overview.jsx';
+import Related from '../../components/Related/Related.jsx';
+import Cards from '../../components/Related/Cards.jsx';
+import Card from '../../components/Related/Card.jsx';
 
 
 // fake product data
+const fakeProducts = [{'id':63609,'campus':'hr-sfo','name':'Camo Onesie','slogan':'Blend in to your crowd','description':'The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.','category':'Jackets','default_price':'140.00','created_at':'2021-12-21T17:19:40.556Z','updated_at':'2021-12-21T17:19:40.556Z'},{'id':63610,'campus':'hr-sfo','name':'Bright Future Sunglasses','slogan':'You\'ve got to wear shades','description':'Where you\'re going you might not need roads, but you definitely need some shades. Give those baby blues a rest and let the future shine bright on these timeless lenses.','category':'Accessories','default_price':'69.00','created_at':'2021-12-21T17:19:40.556Z','updated_at':'2021-12-21T17:19:40.556Z'}];
+
 const fakeProductDetails = {
   'id': 63618,
   'campus': 'hr-sfo',
@@ -31,27 +37,60 @@ it('Title component renders correctly', () => {
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
+
 it('Category component renders correctly', () => {
   const tree = renderer
     .create(<Category product={fakeProductDetails}/>)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
+
 it('Description component renders correctly', () => {
   const tree = renderer
     .create(<Description product={fakeProductDetails}/>)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
+
 it('Reviews component renders correctly', () => {
   const tree = renderer
     .create(<Reviews product={fakeProductDetails}/>)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
+
 it('Price component renders correctly', () => {
   const tree = renderer
     .create(<Price currStyle={fakeStyles.results[0]}/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('Overview component renders correctly', () => {
+  const tree = renderer
+    .create(<Overview productId={63613}/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('Related component renders correctly', () => {
+  const tree = renderer
+    .create(<Related productId={63613}/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('Cards component renders correctly', () => {
+  const tree = renderer
+    .create(<Cards products={fakeProducts} currProduct={fakeProductDetails}/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+
+it('Card component renders correctly', () => {
+  const tree = renderer
+    .create(<Card products={fakeProducts} currProduct={fakeProductDetails}/>)
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
