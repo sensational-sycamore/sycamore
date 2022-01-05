4acf5a850fd819ad261a4bbc365cc35d6c784ee7
Van Hsieh <van.hsieh1@gmail.com>
2022-01-04 23:46:42 -0800

    rendering and click event test for style selector

--- client/src/tests/Overview/dom.test.js
+++ client/src/tests/Overview/dom.test.js
@@ -1,7 +1,26 @@
 import React from 'react';
-import { cleanup, fireEvent, render } from '@testing-library/react';
-import StyleSelector from '../../components/Overview/subcomponents/StyleSelector.jsx';
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act } from 'react-dom/test-utils';
 
+import Title from '../../components/Overview/subcomponents/Title.jsx';
 
-afterEach(cleanup);
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
 
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it('renders with or without a name', () => {
+  act(() => {
+    render(<Title product={{name: 'a product'}}/>, container);
+  });
+  expect(container.textContent).toBe('a product');
+});
