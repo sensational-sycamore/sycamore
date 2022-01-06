ef3d929e829b5a03764e25780fe096619ecdd2da
Van Hsieh <van.hsieh1@gmail.com>
2022-01-06 15:48:40 -0800

    removed unneccessary import from GalleryImages.jsx

--- client/src/components/Overview/subcomponents/GalleryImages.jsx
+++ client/src/components/Overview/subcomponents/GalleryImages.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import css from '../styles/galleryImages.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp, faChevronDown} from '@fortawesome/free-solid-svg-icons';
