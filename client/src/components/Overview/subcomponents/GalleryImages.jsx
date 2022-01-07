f7741f6398f5d8ad05979f93170589bcc24dcb9e
Van Hsieh <van.hsieh1@gmail.com>
2022-01-06 21:09:15 -0800

    added left right button for gallery

--- client/src/components/Overview/subcomponents/GalleryImages.jsx
+++ client/src/components/Overview/subcomponents/GalleryImages.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import css from '../styles/galleryImages.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronUp, faChevronDown} from '@fortawesome/free-solid-svg-icons';
-
+import { faChevronUp, faChevronDown, faChevronLeft, faChevronRight} from '@fortawesome/free-solid-svg-icons';
 
 const GalleryImages = ({ handleImageSelect, photos, currImage}) => {
 
@@ -31,17 +30,24 @@ const GalleryImages = ({ handleImageSelect, photos, currImage}) => {
 
 
   return (
-    <div className={css.imageOptions}>
-      <div onClick={handleUp} >
-        <FontAwesomeIcon color="white" icon={faChevronUp} size="sm"/>
-      </div>
+    <div className={css.imagesContainer}>
+      <span className={css.leftIcon} onClick={handleUp}>
+        <FontAwesomeIcon icon={faChevronLeft} size="lg" color="white"/>
+      </span>
+      <span className={css.rightIcon} onClick={handleDown}>
+        <FontAwesomeIcon icon={faChevronRight} size="lg" color="white"/>
+      </span>
+      <div className={css.imageOptions}>
+        <div onClick={handleUp} className={css.scrollButton}>
+          <FontAwesomeIcon color="white" icon={faChevronUp} size="sm"/>
+        </div>
+        {photos.slice(photosStart, photosEnd).map((photo, index) => (
+          <img className={`${css.eachImage} ${index === selectedImg ? css.selected : null}`} onClick={() => handleSelect(index)} src={photo.thumbnail_url} alt="" key={index}/>
+        ))}
 
-      {photos.slice(photosStart, photosEnd).map((photo, index) => (
-        <img className={`${css.eachImage} ${index === selectedImg ? css.selected : null}`} onClick={() => handleSelect(index)} src={photo.thumbnail_url} alt="" key={index}/>
-      ))}
-
-      <div onClick={handleDown} className={css.scrollButton}>
-        <FontAwesomeIcon color="white" icon={faChevronDown} size="sm"/>
+        <div onClick={handleDown} className={css.scrollButton}>
+          <FontAwesomeIcon color="white" icon={faChevronDown} size="sm"/>
+        </div>
       </div>
     </div>
   );
