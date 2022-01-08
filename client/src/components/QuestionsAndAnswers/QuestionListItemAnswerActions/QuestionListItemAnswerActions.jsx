dbec750a57e41a85809e9f865099c298fd91b601
Fernanda Rodrigues da Silva <fernanda.rodrigues.cdc@gmail.com>
2022-01-08 13:24:02 -0800

    Aplying moment to format the answer date

--- client/src/components/QuestionsAndAnswers/QuestionListItemAnswerActions/QuestionListItemAnswerActions.jsx
+++ client/src/components/QuestionsAndAnswers/QuestionListItemAnswerActions/QuestionListItemAnswerActions.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './QuestionListItemAnswerActions.scss';
 import HelpfulButton from '../../Shared/HelpfulButton/HelpfulButton.jsx';
+import Moment from 'moment';
 
 const QuestionListItemAnswerActions = ({onHelpulButtonClick, author, date, helpfulness, id}) => {
   return (
     <div className="answer-actions">
-      <p>by {author}, {date}</p>
+      <p>by {author}, <span data-testid="date">{Moment(date).format('MMMM Do YYYY')}</span></p>
       <p className="separator">|</p>
       <HelpfulButton
         onHelpulButtonClick={onHelpulButtonClick}
